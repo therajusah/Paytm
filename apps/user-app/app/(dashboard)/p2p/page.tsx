c508709a85a5d589d2f8cf4f1b9388d52a32dd80
therajusah <workwithrajukumar@gmail.com>
2024-11-11 20:13:37 +0530

    Added P2P Transfer sidebar item and updated token generation in createOnRampTransaction function, Schema Update for p2p transfer

---
+++ apps/user-app/app/(dashboard)/p2p/page.tsx
@@ -0,0 +1,8 @@
+import { SendCard } from "../../../components/SendCard";
+
+export default function() {
+    return <div className="w-full">
+        P2P Page
+        <SendCard />
+    </div>
+}
--- apps/user-app/app/lib/actions/createOnRamptxn.ts
+++ apps/user-app/app/lib/actions/createOnRamptxn.ts
@@ -6,7 +6,7 @@ import prisma from "@repo/db/client";
 
 export async function createOnRampTransaction(amount: number, provider: string) {
   const session = await getServerSession(authOptions);
-const token = Math.random().toString();
+const token = (Math.random()*1000).toString();
   const userId = session.user.id;
   if (!userId) {
     return {
